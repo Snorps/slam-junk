0a6e1221cc63ada1370b41e0279fca878ae3536b
Snorps <znellsnorps@outlook.com>
2024-04-02 18:02:13 +0100

    Added grabbing and throwing

--- entity/Hands.gd
+++ entity/Hands.gd
@@ -2,34 +2,40 @@ extends AnimatableBody3D
 
 @export var head: Node3D
 
-@onready var joint = $PinJoint3D
+var grab_object = null
+var grab_force = 10000
+var grab_speed_damping = 0.97
+var throw_force = 1
+
+func _physics_process(delta):
+	if grab_object != null:
+		grab_object.apply_force((global_position - grab_object.global_position) * grab_object.mass * grab_force * delta)
+		grab_object.linear_velocity *= grab_speed_damping
 
 func try_grabthrow():
-	if not is_instance_valid(joint): return
-	if joint.node_b == null:
+	if grab_object != null:
 		throw()
 	else:
 		grab()
 
 func grab():
-	print(joint.global_position)
-	print(global_position)
 	var space_state = get_world_3d().direct_space_state
-	# use global coordinates, not local to node
-	var dir = (global_position - head.global_position).normalized()
-	var query = PhysicsRayQueryParameters3D.create(head.global_position, dir*50)
+	var query = PhysicsRayQueryParameters3D.create(head.global_position, look_vector()*50)
 	var result = space_state.intersect_ray(query)
 	if not result: return
 	if not result.collider is RigidBody3D: return
+	
 	PhysicsServer3D.body_set_state(
 		result.rid,
 		PhysicsServer3D.BODY_STATE_TRANSFORM,
 		Transform3D.IDENTITY.translated(global_position)
 	)
-	#joint = PinJoint3D.new()
-	#add_child(joint)
-	#joint.node_a = self.get_path()
-	joint.node_b = result.collider.get_path()
+	grab_object = result.collider
 
 func throw():
-	joint.node_b == null
+	grab_object.apply_impulse(look_vector() * throw_force)
+	grab_object = null
+
+func look_vector():
+	return (global_position - head.global_position).normalized()
+	
