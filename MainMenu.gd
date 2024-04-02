bc477b9e4636614f2c20412ab4938d17f8582340
Alexis <awinters108@gmail.com>
2024-04-02 18:01:28 +0100

    Added main menu, changed starting scene to said menu

---
+++ MainMenu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+var confirm = false
+
+func _on_start_pressed():
+	var startButton = get_node("Start")
+	if(confirm == false):
+		startButton.text = "Okay but are you sure tho"
+		confirm = true
+	else:
+		get_tree().change_scene_to_file("res://generic_map.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
