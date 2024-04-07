64e000ef530d63944c8fd6ec2d1e6c79cce0fa21
Snorps <znellsnorps@outlook.com>
2024-04-08 00:56:22 +0100

    Added dynamic music

---
+++ entity/trigger_changemusic.gd
@@ -0,0 +1,36 @@
+extends Area3D
+
+var overlaps := 0
+
+var is_pressed = false
+
+var music
+
+func _init() -> void:
+	connect("body_shape_entered", body_shape_entered)
+	connect("body_shape_exited", body_shape_exited)
+
+func body_shape_entered(body_id, body: Node, body_shape_idx: int, self_shape_idx: int) -> void:
+	if not (body == GameStateManager.player):
+		return
+
+	##checks that the entity triggering the goal post is, in fact, baketbol
+	if overlaps == 0:
+		press()
+
+	overlaps += 1
+
+func body_shape_exited(body_id, body: Node, body_shape_idx: int, self_shape_idx: int) -> void:
+	if not (body == GameStateManager.player):
+		return
+
+	overlaps -= 1
+
+func press() -> void:
+	if music != null:
+		AudioManager.set_music(music, null, true)
+
+
+func _func_godot_apply_properties(props: Dictionary):
+	if 'music' in props:
+		music = load(props.music)
--- manager/audio_manager.gd
+++ manager/audio_manager.gd
@@ -2,8 +2,12 @@ extends Node
 
 const music_volume = -15
 
-func set_music(audio: AudioStream, volume = 0.0):
-	$Music.volume_db = int(volume) + music_volume
+func set_music(audio: AudioStream, volume = null, keep_time = false):
+	if volume != null:
+		$Music.volume_db = int(volume) + music_volume
 	if $Music.stream != audio:
+		var playback_time = 0
+		if keep_time == true:
+			playback_time = $Music.get_playback_position()
 		$Music.stream = audio
-		$Music.play()
+		$Music.play(playback_time)
