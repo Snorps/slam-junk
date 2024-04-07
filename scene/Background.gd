317d9dff4ab210b235a5bddd813e0241456f14df
Snorps <znellsnorps@outlook.com>
2024-04-07 22:49:36 +0100

    New menu stuff

--- scene/Background.gd
+++ scene/Background.gd
@@ -1,9 +1,13 @@
 extends MeshInstance3D
 
-var speed = 0.2
+var speed_x = 1
+var speed_y = 0.6
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_surface_override_material(0).uv1_offset.x += delta * speed
+	get_surface_override_material(0).uv1_offset.x += delta * speed_x
 	if(get_surface_override_material(0).uv1_offset.x >= 1):
 		get_surface_override_material(0).uv1_offset.x = 0
+	get_surface_override_material(0).uv1_offset.y += delta * speed_y
+	if(get_surface_override_material(0).uv1_offset.y >= 1):
+		get_surface_override_material(0).uv1_offset.y = 0
