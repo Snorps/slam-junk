d1a8416f1d3c7480e4ec9dc3e86595b8f2bbdb27
Snorps <znellsnorps@outlook.com>
2024-04-08 20:50:22 +0100

    Made ball go into net properly

--- entity/trigger_goal.gd
+++ entity/trigger_goal.gd
@@ -2,8 +2,7 @@ extends Area3D
 
 signal trigger()
 
-var is_pressed = false
-var trigger_signal_delay :=  0.0
+var trigger_signal_delay :=  0.5
 
 var overlaps := 0
 
@@ -32,18 +31,15 @@ func body_shape_exited(body_id, body: Node, body_shape_idx: int, self_shape_idx:
 	overlaps -= 1
 
 func press() -> void:
-	if is_pressed:
-		return
-
-	is_pressed = true
-
 	emit_trigger()
 
 func emit_trigger() -> void:
 	await get_tree().create_timer(trigger_signal_delay).timeout
 	GameStateManager.win()
-	#trigger.emit()
+	trigger.emit()
 
 func _func_godot_apply_properties(props: Dictionary):
 	if 'trigger_signal_delay' in props:
 		trigger_signal_delay = props.trigger_signal_delay
+	if "target" in props:
+		EntityTargetManager.register_emitter(self, props.target)
