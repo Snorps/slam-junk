ae7c4ce1252385b3203dd34f408f4857e34779fa
Alexis <awinters108@gmail.com>
2024-04-09 17:39:09 +0100

    Fixed resolution scaling on UI elements

--- scene/MainMenu.gd
+++ scene/MainMenu.gd
@@ -22,17 +22,18 @@ func _on_start_pressed():
 
 var rng = RandomNumberGenerator.new()
 var time = 0
-@onready var title_base_scale = $Title.scale
+@onready var title_base_scale = get_node("Node3D/Camera3D/Title").scale
 func _process(delta):
 	time += delta
 	var bounce = sin(time*50)/50
-	$Title.scale = title_base_scale + Vector2(bounce, bounce)
+	get_node("Node3D/Camera3D/Title").scale = title_base_scale + Vector2(bounce, bounce)
 	var rot = ball_speed * delta
 	if rng.randf_range(0.0, 1.0) > 1-ball_glitch_chance:
 		var rand = rng.randf_range(0, 10.0)
 		var sign = rng.randi_range(0,1) * 2 - 1
 		rot += sign * (-log(-rand + 10) * ball_aggression + ball_aggression)
 	$Node3D/TheBall.rotation.y += rot
+
 
 func _on_quit_pressed():
 	get_tree().quit()
