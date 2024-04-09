31a5b82c93a278b2b9aab7ca4d5f6c94c03a221f
Alexis <awinters108@gmail.com>
2024-04-09 13:11:21 +0100

    Updated shop UI. Added check for maximum upgrade

--- ui/vending_machine_gui.gd
+++ ui/vending_machine_gui.gd
@@ -1,6 +1,14 @@
 extends Control
 
+
+var performoLabel
+var necroLabel
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _ready():
+	performoLabel = get_node("PerformoLabel")
+	necroLabel = get_node("NecroLabel")
+	
 func _process(delta):
 	pass
 
@@ -12,6 +20,7 @@ func _on_performosport_pressed():
 		print(Flags.performosport)
 	GameStateManager.player.update_fov()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	performoLabel.text = "Moneycosts: five fives \nAmountsof: " + str(Flags.performosport) + "\nMAXOPERFORMO: FIVE"
 	hide()
 	
 
