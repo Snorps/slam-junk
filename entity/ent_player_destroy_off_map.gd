aaf876c9974c180e11d5a6cfbc2a3a6fac01f341
Snorps <znellsnorps@outlook.com>
2024-08-06 11:09:44 +0100

    Fixed error ending

--- entity/ent_player_destroy_off_map.gd
+++ entity/ent_player_destroy_off_map.gd
@@ -3,4 +3,4 @@ extends DestroyOffMap
 
 # Called when the node enters the scene tree for the first time.
 func on_destroy():
-	get_tree().change_scene_to_file("res://scene/Ending.tscn")
+	get_tree().change_scene_to_file("res://scene/ending.tscn")
